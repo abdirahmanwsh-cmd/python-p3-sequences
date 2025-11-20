197df30c0fed5238810cb91c369c86d200d01e6f
Manka.code <Abdirahman.wsh@gmail.com>
2025-11-20 07:12:15 -0500

    adds sequences.py

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    my_list = []
+    a, b = 0, 1
+    for _ in range(length):
+        my_list.append(a)
+        a, b = b, a + b
+
+    print(my_list)
+
     pass
